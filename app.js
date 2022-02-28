ce9f629a219b0a72ecf3c47f1ac192447d29d659
HP 840 <yasin80545859@.com>
2022-02-28 14:51:50 +0600

    third

--- app.js
+++ app.js
@@ -3,6 +3,36 @@ const allPlayer = () =>{
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchVelue}`
     fetch(url)
     .then(res => res.json())
+    .then(data => showPlyer(data.player))
+    // console.log(url)
+}
+const showPlyer = players =>{
+    for(const player of players){
+        const parentContainer = document.getElementById('parent-container')
+    const div = document.createElement('div')
+    div.innerHTML=`
+    <div class="card p-5">
+        <div class="pro-pic">
+            <img class="w-50" src="${player.strThumb}" alt="">
+                </div>
+                <h2>Name: ${player.strPlayer} </h2>
+                <h5>Country: ${player.strNationality}</h5>
+                <p></p>
+                <div class="allButton">
+                    <button class="btn btn-danger">Delet</button>
+                    <button onclick="details('${player.idPlayer}')" class="btn btn-success">Detail</button>
+                </div>
+            </div>
+    `
+    parentContainer.appendChild(div)
+    console.log(parentContainer)
+    }
+    
+}
+const details = (info) =>{
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${info}`
+    fetch(url)
+    .then(res => res.json())
     .then(data => console.log(data))
-    console.log(url)
+    // console.log('o)
 }
